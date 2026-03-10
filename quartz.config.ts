4437ff6f88e7b2049208d5a19796d26df2fe83f2
jempolbagas <aditamabagas.13@gmail.com>
2026-03-10 11:30:24 +0700

    Quartz sync: Mar 10, 2026, 11:30 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "99_Meta/Templates", "00_Daily_Notes"],
+    ignorePatterns: ["private", "templates", ".obsidian", "99_Meta/Templates", "00_Daily_Notes", "30_Resources", ".trash"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
